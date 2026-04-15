45b289c9e3982a32dd1dc74c0a40ec4d993a17a5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-15 09:16:18 +0530

    add comment to accept client conn

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char **argv)
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std::cout << "Logs from your program will appear here!\n";
 
-    // Uncomment the code below to pass the first stage
+    // Accept a client connection
     accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
     std::cout << "Client connected\n";
 
