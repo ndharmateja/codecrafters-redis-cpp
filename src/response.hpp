296cb6a9f04f1c0cc8df5eef63f66ff5582ce31f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-16 20:50:56 +0530

    refactor the Response classes

--- src/response.hpp
+++ src/response.hpp
@@ -5,34 +5,32 @@
 
 class Response
 {
+private:
+    inline static const std::string CRLF{"\r\n"};
+
+    // We are returning a reference to the response itself so that we can directly return
+    // the output of add_item in the create_bulk_string etc
+    static std::string &add_item(std::string &response, const std::string &item) { return response.append(item).append(CRLF); }
+    static std::string &add_item(std::string &response, size_t item) { return response.append(std::to_string(item)).append(CRLF); }
+
 public:
     static std::string create_bulk_string(const std::string &input)
     {
-        std::string response;
-        response.append("$");
-        response.append(std::to_string(input.length()));
-        response.append("\r\n");
-        response.append(input);
-        response.append("\r\n");
-        return response;
+        std::string response{"$"};
+        add_item(response, input.length());
+        return add_item(response, input);
     }
 
     static std::string create_simple_string(const std::string &input)
     {
-        std::string response;
-        response.append("+");
-        response.append(input);
-        response.append("\r\n");
-        return response;
+        std::string response{"+"};
+        return add_item(response, input);
     }
 
     static std::string create_simple_error(const std::string &input)
     {
-        std::string response;
-        response.append("-");
-        response.append(input);
-        response.append("\r\n");
-        return response;
+        std::string response{"-"};
+        return add_item(response, input);
     }
 
     static std::string create_null_bulk_string() { return "$-1\r\n"; };
