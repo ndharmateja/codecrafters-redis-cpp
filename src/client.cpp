5eda74a3a75e1f3f60af502578dde02d3672b4aa
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-16 20:30:45 +0530

    update the lowercasing of command and Response class in client

--- src/client.cpp
+++ src/client.cpp
@@ -3,6 +3,7 @@
 
 #include "client.hpp"
 #include "util.hpp"
+#include "response.hpp"
 
 // Constructor and destructor
 Client::Client(int client_fd) : fd{client_fd}, buf{1024} {}
@@ -30,23 +31,19 @@ void Client::run()
         std::string response;
 
         // Handle PING command
-        if (command.front() == "PING")
-            response.append("+PONG\r\n");
+        if (command.front() == "ping")
+            response.append(Response::create_bulk_string("PONG"));
 
         // Handle ECHO command with one argument
         // Send response back as a RESP bulk string
-        else if (command.front() == "ECHO")
-        {
-            response.append("$");
-            response.append(std::to_string(command[1].length()));
-            response.append("\r\n");
-            response.append(command[1]);
-            response.append("\r\n");
-        }
+        else if (command.front() == "echo")
+            response.append(Response::create_bulk_string(command[1]));
+
+        // Handle SET command
 
         // All other commands are unsupported
         else
-            response.append("-Invalid/unsupported command\r\n");
+            response.append(Response::create_simple_error("Invalid/unsupported command"));
 
         // Send the response
         std::cout << "Raw response: '";
