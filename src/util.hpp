2d26d607c79d630e03ef3c8f095f8db6a519e025
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-16 12:04:13 +0530

    add functions to print escaped version of string and vector

--- src/util.hpp
+++ src/util.hpp
@@ -100,3 +100,39 @@ std::vector<std::string> parse_command(Buffer &buf)
     buf.set_read_pos(curr_pos);
     return parts;
 }
+
+/**
+ * Function to print a string with escaped characters
+ */
+void print_escaped(const std::string &str)
+{
+    for (char c : str)
+    {
+        switch (c)
+        {
+        case '\r':
+            std::cout << "\\r";
+            break;
+        case '\n':
+            std::cout << "\\n";
+            break;
+        default:
+            std::cout << c;
+            break;
+        }
+    }
+}
+
+void print_vector(const std::vector<std::string> &vec)
+{
+    std::cout << "[";
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        std::cout << "\"" << vec[i] << "\"";
+        if (i < vec.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]";
+}
