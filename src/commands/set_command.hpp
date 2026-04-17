efa8494f00dea6650a8e776d03c26f0ef9360143
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-17 14:23:29 +0530

    fix error handling and expiry in ms for set command

--- src/commands/set_command.hpp
+++ src/commands/set_command.hpp
@@ -13,21 +13,21 @@ class SetCommand : public Command
 private:
     std::string key;
     std::string value;
-    std::optional<std::chrono::steady_clock::time_point> expiry_time;
+    std::optional<long long> expiry_in_ms;
 
 public:
     explicit SetCommand(const std::vector<std::string> &command_parts)
     {
         // Parse key and value
-        if (command_parts.size() < 3)
-            throw InvalidCommandStructureError("SET should have atleast 3 parts.");
+        if (command_parts.size() != 3 || command_parts.size() != 5)
+            throw InvalidCommandStructureError("SET should have exactly 3 or 5 parts.");
         key = command_parts[1];
         value = command_parts[2];
 
         // parse EX or PX args
-        if (command_parts.size() <= 4)
+        if (command_parts.size() == 3)
         {
-            expiry_time = std::nullopt;
+            expiry_in_ms = std::nullopt;
             return;
         }
 
@@ -36,7 +36,6 @@ public:
             throw InvalidCommandStructureError("SET only supports EX/PX args.");
 
         // parse the number
-        long long expiry_in_ms;
         try
         {
             expiry_in_ms = std::stoll(command_parts[4]);
@@ -48,16 +47,13 @@ public:
 
         // If EX, then multiply the time by 1000 to get it in ms
         if (strcasecmp("EX", command_parts[3].c_str()))
-            expiry_in_ms *= 1000;
-
-        // Store the expiry time as a timestamp
-        expiry_time = std::chrono::steady_clock::now() + std::chrono::milliseconds(expiry_in_ms);
+            expiry_in_ms = expiry_in_ms.value() * 1000;
     }
 
     std::string execute() override
     {
         KeyValueStore &store = KeyValueStore::get_instance();
-        store.set_value(key, value);
+        store.set_value(key, value, expiry_in_ms);
         return Response::create_simple_string("OK");
     }
 };
