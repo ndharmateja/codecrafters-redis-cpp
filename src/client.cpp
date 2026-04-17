17490f36c19ef2efa68bf8657429f6d1a5ee8a5d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-17 14:00:47 +0530

    refactor to use the command factory

--- src/client.cpp
+++ src/client.cpp
@@ -41,40 +41,9 @@ void Client::run()
         // Create and send back the response
         std::string response;
 
-        // Handle PING command
-        if (command.front() == "ping")
-            response.append(Response::create_simple_string("PONG"));
-
-        // Handle ECHO command with one argument
-        // Send response back as a RESP bulk string
-        else if (command.front() == "echo")
-            response.append(Response::create_bulk_string(command[1]));
-
-        // Handle SET command
-        else if (command.front() == "set")
-        {
-            KeyValueStore &store = KeyValueStore::get_instance();
-            store.set_value(command[1], command[2]);
-            response.append(Response::create_simple_string("OK"));
-        }
-
-        // Handle GET command
-        else if (command.front() == "get")
-        {
-            KeyValueStore &store = KeyValueStore::get_instance();
-            std::optional<std::string> result = store.get_value(command[1]);
-
-            // If value exists, we send that corresponding value otherwise
-            // we send the null string back
-            response.append(
-                result.has_value()
-                    ? Response::create_bulk_string(result.value())
-                    : Response::create_null_bulk_string());
-        }
-
-        // All other commands are unsupported
-        else
-            response.append(Response::create_simple_error("Invalid/unsupported command"));
+        // Create command and response
+        std::unique_ptr<Command> cmd = CommandFactory::create_command(command);
+        response = cmd->execute();
 
         // Send the response
         std::cout << "Raw response: '";
