1b78b9b6ae538c0644f5044c3881a8b51f886811
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-18 10:09:42 +0530

    modify rpush command to accept a list of values

--- src/commands/rpush_command.hpp
+++ src/commands/rpush_command.hpp
@@ -3,6 +3,7 @@
 
 #include <optional>
 #include <vector>
+#include <deque>
 #include "command.hpp"
 #include "../key_value_store.hpp"
 #include "../response.hpp"
@@ -12,16 +13,23 @@ class RpushCommand : public Command
 {
 private:
     std::string key;
-    std::string value;
+    std::deque<std::string> values;
 
 public:
     explicit RpushCommand(const std::vector<std::string> &command_parts)
     {
-        // Parse key and value
-        if (command_parts.size() != 3)
-            throw InvalidCommandStructureError("RPUSH should have exactly 3 parts.");
+        // Parse key and values
+        if (command_parts.size() < 3)
+            throw InvalidCommandStructureError("RPUSH should have atleast 3 parts.");
         key = command_parts[1];
-        value = command_parts[2];
+
+        // Add all the values to the deque
+        auto it = command_parts.begin() + 2;
+        while (it != command_parts.end())
+        {
+            values.push_back(*it);
+            it++;
+        }
     }
 
     std::string execute() override
@@ -30,7 +38,7 @@ public:
 
         // If value exists, we send that corresponding value otherwise
         // we send the null string back
-        return Response::create_integer_string(store.push_back_list_value(key, value));
+        return Response::create_integer_string(store.push_back_list_values(key, values));
     }
 };
 
